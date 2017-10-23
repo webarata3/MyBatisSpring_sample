78bda66baeac1db7d68ea807599a153eed2b094f
webarata3 <webarata3@gmail.com>
2017-10-23 19:23:57 +0900

    インターフェースを使わないように変更

--- MyBatisSpring03/src/main/java/com/example/mybatis/Main.java
+++ MyBatisSpring03/src/main/java/com/example/mybatis/Main.java
@@ -1,13 +1,11 @@
 package com.example.mybatis;
 
-import com.example.mybatis.mapper.BookMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,8 +15,7 @@ public class Main {
         }
 
         try (SqlSession session = sqlSessionFactory.openSession()) {
-            BookMapper bookMapper = session.getMapper(BookMapper.class);
-            Book book = bookMapper.select(101);
+            Book book = session.selectOne("com.example.mybatis.mapper.BookMapper.select", 101);
             System.out.println(book);
         }
     }
