6391acbc97d78f321a5443513c746e5d75610fcb
webarata3 <webarata3@gmail.com>
2017-11-11 03:40:01 +0900

    Gradleのバージョンアップ

--- MyBatisSpring06/src/main/java/com/example/mybatis/mapper/BookMapper.java
+++ MyBatisSpring06/src/main/java/com/example/mybatis/mapper/BookMapper.java
@@ -1,9 +1,13 @@
 package com.example.mybatis.mapper;
 
 import com.example.mybatis.Book;
+import org.apache.ibatis.annotations.Insert;
 
 import java.util.List;
 
 public interface BookMapper {
     public List<Book> selectAll();
+
+    @Insert("INSERT INTO book VALUES(#{bookId}, #{bookName}, #{author}, #{price}")
+    public int insert(Book book);
 }
