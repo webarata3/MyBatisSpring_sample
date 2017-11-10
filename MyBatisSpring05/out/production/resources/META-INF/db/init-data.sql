85dbe2dd453f35e21e7dc8e75c8a2b1f8ac93a6e
webarata3 <webarata3@gmail.com>
2017-11-10 16:16:02 +0900

    不要ファイルの除去

--- MyBatisSpring05/out/production/resources/META-INF/db/init-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO book VALUES(101, 'よく分かるSpring', '山田太郎', 3980);
-INSERT INTO book VALUES(102, '難しいSpring', '佐藤花子', 2999);
--- MyBatisSpring05/out/production/resources/META-INF/db/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE book(
-  book_id INT,
-  book_name VARCHAR(80),
-  author VARCHAR(40),
-  price INT,
-  PRIMARY KEY(book_id)
-);
