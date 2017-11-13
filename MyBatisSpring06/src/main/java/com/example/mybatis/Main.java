49feb6b4833db79635b3d5ec44cb9be629a85e05
webarata3 <webarata3@gmail.com>
2017-11-13 17:19:48 +0900

    不要なimportの削除

--- MyBatisSpring06/src/main/java/com/example/mybatis/Main.java
+++ MyBatisSpring06/src/main/java/com/example/mybatis/Main.java
@@ -4,8 +4,6 @@ import com.example.mybatis.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/META-INF/application-context.xml");
