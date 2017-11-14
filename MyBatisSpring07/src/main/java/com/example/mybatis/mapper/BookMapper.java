dfaf9783fd1bc52c25cd701d15e5870f737e93bb
webarata3 <webarata3@gmail.com>
2017-11-14 14:18:31 +0900

    MyBatisSpring07追加

---
+++ MyBatisSpring07/src/main/java/com/example/mybatis/mapper/BookMapper.java
@@ -0,0 +1,7 @@
+package com.example.mybatis.mapper;
+
+import com.example.mybatis.Book;
+
+public interface BookMapper {
+    public int insert(Book book);
+}
--- MyBatisSpring07/src/main/java/com/example/mybatis/service/BookService.java
+++ MyBatisSpring07/src/main/java/com/example/mybatis/service/BookService.java
@@ -1,6 +1,7 @@
 package com.example.mybatis.service;
 
 import com.example.mybatis.Book;
+import com.example.mybatis.mapper.BookMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService {
     @Autowired
-    private SqlSession sqlSession;
+    private BookMapper bookMapper;
 
     public void insertErrorTest() {
         Book book = new Book(20, "MyBatis", "田中一郎", 999);
-        sqlSession.insert("com.example.mybatis.mapper.BookMapper.insert", book);
-        sqlSession.insert("com.example.mybatis.mapper.BookMapper.insert", book);
+        bookMapper.insert(book);
+        bookMapper.insert(book);
     }
 }
